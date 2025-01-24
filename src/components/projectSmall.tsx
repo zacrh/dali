33d6163b87287ae1c13f31a7a2f159eb5502cfac
Zachary Rosca-Halmagean <zac.princeton@gmail.com>
2025-01-24 16:06:21 -0500

    clean up small project component

--- src/components/projectSmall.tsx
+++ src/components/projectSmall.tsx
@@ -37,11 +37,9 @@ export default function ProjectSmall({ projectData, session }: ProjectProps) {
                         <h3 className="text-md font-semibold gap-2 flex items-center">{project.name} <span className="font-medium text-gray-500 dark:text-slate-500">#{project.alias}</span></h3>
                     </div>
                 </div>
-                
                 <div className="flex p-1.5 rounded-md cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-right text-gray-600 dark:text-slate-500"><path d="m9 18 6-6-6-6"/></svg>
                 </div>
-                
             </div>
         </Link>
     )
