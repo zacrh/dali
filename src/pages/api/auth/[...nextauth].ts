aae5d89429089c1b91e87c71b06ae5e6e398415c
Zachary Rosca-Halmagean <zac.princeton@gmail.com>
2025-01-26 18:31:36 -0500

    fix type issue and build command

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -44,7 +44,7 @@ export const authOptions: NextAuthOptions = {
         url,
         provider: { server, from },
       }) {
-        sendVerifyRequest({ identifier: email, url: url, provider: { server: server, from: from } })
+        sendVerifyRequest({ identifier: email, url: url, provider: { server: server as string, from: from } })
       },
     }),
   ],
