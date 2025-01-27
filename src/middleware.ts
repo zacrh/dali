5e91dba4dc4da3165dae370bbf0fd2183bb811cf
Zachary Rosca-Halmagean <zac.princeton@gmail.com>
2025-01-26 23:48:41 -0500

    try this

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,4 +11,4 @@ export default async function middleware(req: NextRequest) {
   return NextResponse.next();
 }
 
-export const config = { matcher: ["/onboarding"] }
+export const config = { matcher: [] }
