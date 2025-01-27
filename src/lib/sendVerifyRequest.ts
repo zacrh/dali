1952a1f9b088f7090107ee8422a527b408410685
Zachary Rosca-Halmagean <zac.princeton@gmail.com>
2025-01-26 19:38:16 -0500

    update smtp port again idg why this is so finnicky

--- src/lib/sendVerifyRequest.ts
+++ src/lib/sendVerifyRequest.ts
@@ -18,8 +18,7 @@ export async function sendVerifyRequest(params: Params) {
     console.log(provider.server, provider.from, host, identifier, url, provider)
     const transport = createTransport({
         host: 'smtp.resend.com',
-        port: 465,
-        secure: true,
+        port: 587,
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD,
