ccb1a054324f5862c185c6dfc99cfb1d97f665a2
Zachary Rosca-Halmagean <zac.princeton@gmail.com>
2025-01-26 23:29:22 -0500

    page title and favicon

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import LeftNav from "@/components/left-nav";
 import RightBar from "@/components/right-bar";
 import { useSession } from "next-auth/react";
+import Head from 'next/head'; 
 
 interface LayoutProps {
     children: ReactNode;
@@ -13,7 +14,11 @@ export default function Layout({ children }: LayoutProps) {
 
 
     return (
+        
         <div className="flex h-screen w-screen items-center justify-center">
+            <Head>
+                <title>Dalibook</title>
+            </Head>
             <div className="flex flex-row w-full h-full max-w-6xl justify-center">
                 <LeftNav session={session} />
                 {children}
